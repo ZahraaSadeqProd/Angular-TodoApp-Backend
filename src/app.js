dfc2018d9a29702ac00b91b009d02b1c6e3fa7fe
Zahraa Sadeq <zahraasadeq@Zahraas-MacBook-Air.local>
2026-01-31 12:01:39 -0700

    fixing linking issues and environmental issues

--- src/app.js
+++ src/app.js
@@ -18,6 +18,8 @@ const corsOptions = {
     'http://localhost:3000'
   ],
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   optionsSuccessStatus: 200
 };
 
